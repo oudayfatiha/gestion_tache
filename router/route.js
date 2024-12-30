ca0536ecdb1d5897b51e2424d06c3d8cdc5405f3
oudayfatiha <fatihaouday174@gmail.com>
2024-12-30 10:41:49 +0100

    modification sur route

--- router/route.js
+++ router/route.js
@@ -1,58 +1,62 @@
-const express = require('express');
+
+const express = require("express")
 const route = express.Router();
-const Task = require('./../models/task'); 
+const Task = require('./../models/task');
 const bodyParser = require('body-parser');
 
-route.get('/task',async (req,res)=>{
-    try{
-        
-        await Task.find({}).then(resultat=>{
-            res.send(resultat) 
+
+
+
+
+route.get('/task', async (req, res) => {
+    console.log("Request comme from GET");
+    try {
+        await Task.find({}).then(result => {
+            res.send(result)
         })
-
-    }catch(err){
-        console.log(err) 
+    } catch (err) {
+        console.log(err)
     }
-   
-})
-
-route.post('/Task',async (req,res)=>{
-    try{ 
-        const{name,description,completed}=req.body;
-    let new_Task = new Task({
-        title:req.body.title,
-        description:req.body.description,
-        completed:req.body.completed
-    }) 
-    await new_Task.save()
-    res.send('ajoutter avec succes')
-}catch(err){
-    console.log(err)
-}
-})
-
-route.delete('/delet/:id',async(req,res) => {
-    await   Task.findByIdAndDelete(req.params.id)
-    res.send("supprimer  avec succes")
-   })
-
-
-   route.put('/update/:idup', async (req, res) => {
+});
+route.post('/ajouter', async (req, res) => {
+    console.log("Request comme from POST");
+    try {
+        let new_task = new Task({
+            title: req.body.title,
+            description: req.body.description,
+            completed: req.body.completed,
+        });
+        await new_task.save();
+        res.send('save valid');
+    } catch (err) {
+        console.log(err)
+    }
+});
+route.delete('/delete/:id', async (req, res) => {
+    console.log("Request comme from DELETE");
+    try {
+        await Task.findByIdAndDelete(req.params.id)
+        res.send("supprimer  avec succes")
+    } catch (err) {
+        res.send(err)
+    }
+});
+route.put('/update/:id', async (req, res) => {
+    console.log("Request comme from PUT");
     try {
         await Task.findOneAndUpdate(
-            { _id: req.params.idup },
+            { _id: req.params.id },
             {
-                name: req.body.name,
+                title: req.body.title,
                 description: req.body.description,
                 completed: req.body.completed,
             }
         );
         res.send("Modifier avec succès");
     } catch (err) {
-        res.status(500).send(err); 
+        res.send(err)
     }
 });
 
-  
 
-module.exports=route
+module.exports = route;
