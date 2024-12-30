d153d882f99713242aea702a341aa504e95deae0
oudayfatiha <fatihaouday174@gmail.com>
2024-12-30 10:36:13 +0100

    modification  sur index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const mongoose = require('mongoose');
+const express = require("express")
+const mongoose = require("mongoose");
 const route = require('./router/route'); 
 const bodyParser  = require("body-parser");
 
@@ -14,13 +14,14 @@ const connectDB = async () => {
         console.log("Database connected successfully");
     } catch (err) {
         console.error("Database connection error:", err);
-        process.exit(1);
+       
     }
     
 };
 
+
 connectDB();
-app.listen(3000,()=> console.log("serveur  run succes"))
+app.listen(3000,()=>console.log("server done"));
+
+
  
-
-
